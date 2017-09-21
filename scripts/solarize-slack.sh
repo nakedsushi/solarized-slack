235fbfe252c8d8d0a55eda59c372b4bc5ed0deeb
Louise Yang <louise.m.yang@gmail.com>
2017-09-21 08:48:07 -0700

    More tweaks with sidebar colors

--- scripts/solarize-slack.sh
+++ scripts/solarize-slack.sh
@@ -2,6 +2,7 @@
 
 # Prod: https://cdn.rawgit.com/nakedsushi/solarized-slack/master/css/solarized.css
 # Dev: https://raw.githubusercontent.com/nakedsushi/solarized-slack/master/css/solarized.css
+
 cat << 'EOF' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 document.addEventListener('DOMContentLoaded', function() {
  $.ajax({
